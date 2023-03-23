46a78ac3bb542f67bc7a670159de73cb3f573033
Divey Anand <csediveyanand@gmail.com>
2023-03-23 16:23:39 +0530

    commit 1

--- TutortAcademy/ArrayPractise+23Mar2023.py
+++ TutortAcademy/ArrayPractise+23Mar2023.py
@@ -1,15 +1,28 @@
 # 1299. Replace Elements with Greatest Element on Right Side
 class Solution:
     def replaceElements(self, arr: list[int]) -> list[int]:
-        maxnum = 0
-        res = []
-        for i in range(len(arr)):
-            for j in range(i+1,len(arr)):
-                if arr[j] > maxnum:
-                    maxnum = arr[j]
-            res.append(maxnum)
-            maxnum = 0
-        res[-1]=-1
-        return res
+        #TC O(N^2)
+        # maxnum = 0
+        # res = []
+        # for i in range(len(arr)):
+        #     for j in range(i+1,len(arr)):
+        #         if arr[j] > maxnum:
+        #             maxnum = arr[j]
+        #     res.append(maxnum)
+        #     maxnum = 0
+        # res[-1]=-1
+        # return res
+        
+        #TC O(N)
+        # maxonright = -1
+        # for i in range(len(arr)-1,-1,-1):
+        #     curr = arr[i]
+        #     arr[i] = maxonright
+        #     if curr > maxonright:
+        #         maxonright = curr
+        # return arr
+            
+        
+
 mysol = Solution()
 print(mysol.replaceElements(arr = [17,18,5,4,6,1]))
