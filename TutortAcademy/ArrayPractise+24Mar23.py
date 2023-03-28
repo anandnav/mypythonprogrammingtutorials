a85094b12cff608fe3f6f4d16442d3bc1bd846b3
Divey Anand <csediveyanand@gmail.com>
2023-03-28 17:21:14 +0530

    280323_01

--- TutortAcademy/ArrayPractise+24Mar23.py
+++ TutortAcademy/ArrayPractise+24Mar23.py
@@ -33,8 +33,57 @@ class Solution:
         # print(majorityele)
         #Medium 229 Majority Element 2
         #Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times.
-
+        """
+        TC = O(n^2)
+        r = len(nums)//3
+        res = []
+        for i in nums:
+            if (i not in res) and nums.count(i) > r:
+                res.append(i) 
+        return res 
+        """
+        # TC = O(n)
+        # freq = {}
+        # r = len(nums)//3
+        # res = []
+        # for i in nums:
+        #     freq[i]= 1+freq.get(i,0)
+        #     if freq[i] > r and (i not in res):
+        #         res.append(i)
+        # return res
+        # Morrey's Voting Algorithm for TC O(n) and SC O(1)
+        num1,num2 = None,None
+        count1,count2=0,0
+        for num in nums:
+            if num == num1:
+                count1 +=1
+            elif num == num2:
+                count2 += 1
+            elif count1 == 0:
+                num1 = num
+                count1 = 1
+            elif count2 == 0:
+                num2 = num
+                count2 = 1
+            else:
+                count1 -=1
+                count2 -= 1
+        
+        count1,count2 = 0,0
+        for num in nums:
+            if num == num1:
+                count1 += 1
+            elif num == num2:
+                count2 += 1
+        
+        n = len(nums)
+        res = []
+        if count1 > n//3:
+            res.append(num1)
+        if count2 > n//3:
+            res.append(num2)
+        return res    
         
 mysol = Solution()
 #mysol.findArray(pref = [5,2,0,3,1])
-mysol.majorityElement(nums = [3,3,4])
+print(mysol.majorityElement(nums = [2,2,1,1,5,6,5,5]))
